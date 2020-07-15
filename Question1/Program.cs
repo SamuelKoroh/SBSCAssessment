dd085011e017388f8e5c6244e08bdb06ab9f68ed
samuelkoroh <samuelkoroh@gmail.com>
2020-07-15 10:07:43 +0100

    initial commit

---
+++ Question1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Question1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter hyphen separated integers");
+
+            var hyphenNumbers = Console.ReadLine();
+            var numberArray = hyphenNumbers.Split("-");
+
+            if(numberArray.Length > numberArray.Distinct().Count())
+                Console.WriteLine(false);
+            else
+                Console.WriteLine(true);
+
+        }
+    }
+}
---
+++ Question3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Question3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string word's");
+            var word = Console.ReadLine();
+
+            if (!string.IsNullOrWhiteSpace(word))
+            {
+                var dictionary = new Dictionary<string, int>();
+                var result = string.Empty;
+
+                foreach (var ch in word)
+                {
+                    if (dictionary.ContainsKey(ch.ToString()))
+                        dictionary[ch.ToString()]++;
+                    else
+                        dictionary.Add(ch.ToString(), 1);
+                }
+
+
+                foreach (var i in dictionary)
+                    result += i.Key + i.Value;
+
+                Console.WriteLine(result);
+
+            }
+
+            else
+                Console.WriteLine("Please enter a valid word");
+
+        }
+
+
+    }
+}
